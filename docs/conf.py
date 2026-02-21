a8b0cddafa48e685b3734fc32fe552fcb3a1f5ae
Amaan Ul Haq Siddiqui <amaanulhaq.siddiqui@sudoconsultants.com>
2026-02-21 20:50:06 +0500

    chore: add initial Sphinx documentation configuration and structure

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'kfix'
+copyright = '2026'
+author = ''
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+html_static_path = ['_static']
